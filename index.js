9394fb5c51410fe35921f28ccb253f9b7b071c98
twomeetings <2326220@163.com>
2021-12-25 06:44:30 +0800

    use babel/core

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-import * as parser from '@babel/parser'
-import traverse from '@babel/traverse'
-import generate from '@babel/generator'
+import * as babel from '@babel/core'
+
+function myFirstPlugin() {
+  return {
+    visitor: {
+      Identifier(path) {
+        if (path.isIdentifier({ name: 'n' })) {
+          path.node.name = 'x'
+        }
+      },
+    },
+  }
+}
 
 const code = 'const n=1'
-const ast = parser.parse(code)
 
-traverse(ast, {
-  enter(path) {
-    if (path.isIdentifier({ name: 'n' })) {
-      path.node.name = 'x'
-    }
-  },
+const output = babel.transformSync(code, {
+  plugins: [myFirstPlugin],
 })
 
-const output = generate(ast, code)
 console.log(`原代码： 
 ${code}
 转换后代码: 
